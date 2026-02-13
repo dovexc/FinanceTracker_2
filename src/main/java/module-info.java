970c20465ec745c2b7842d0c7062952e8f40440f
EliasSchwebach <eliasschwebach@icloud.com>
2026-02-13 10:00:13 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.financetracker_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.financetracker_2 to javafx.fxml;
+    exports org.example.financetracker_2;
+}
